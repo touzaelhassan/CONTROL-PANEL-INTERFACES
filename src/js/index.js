be40c0b22f23cb4c619f7350cb7c438b27b0fcc3
touza <touzaelhassan@gmail.com>
2022-03-28 20:31:12 +0000

    hide dashboard sidebar on small screens

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,17 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.rtl.min.css';
 import 'normalize.css/normalize.css';
 import '../sass/style.scss';
+
+const sidebarToggler1 = document.querySelector('.sidebar-toggler-1');
+const sidebarToggler2 = document.querySelector('.sidebar-toggler-2');
+const sidebarComponent = document.querySelector('.sidebar-component');
+
+sidebarToggler1.addEventListener('click', function () {
+  sidebarComponent.classList.toggle('active');
+  this.style.display = 'none';
+});
+
+sidebarToggler2.addEventListener('click', function () {
+  sidebarComponent.classList.toggle('active');
+  sidebarToggler1.style.display = 'block';
+});
